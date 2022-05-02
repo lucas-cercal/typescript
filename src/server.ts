91fc1462d93b0f38103be1c7e1789427e0c950b7
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-05-02 20:27:32 -0300

    rota para criar curso com type

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import CreateCourseService from './CreateCourseService';
+import { createCourse } from './routes';
 
 const app = express();
 
 app.get("/", (request, response) => {
   return response.json({message: "Hello World Ignite!"});
 });
+
+app.get("/create", createCourse);
 
 app.listen(3333);
